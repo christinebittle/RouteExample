8f0120cd1b1b6317240fd978984d01486a6300ef
Christine <Christine@CHRISTINE_PC>
2024-09-20 14:34:29 -0400

    Add project files.

---
+++ RouteExample/Controllers/InputsController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputsController : ControllerBase
+    {
+
+        /// <summary>
+        /// This method receives a number and outputs the lucky version of that number
+        /// </summary>
+        /// <param name="UserInput">The number to make lucky</param>
+        /// <returns>The lucky number (UserInput * 3)</returns>
+        /// <example>
+        /// GET : https://localhost:xx/api/Inputs/Lucky/90 -> 270
+        /// GET : https://localhost:xx/api/Inputs/Lucky/30 -> 90
+        /// </example>
+        [HttpGet(template:"Lucky/{UserInput}")]
+        public int Lucky(int UserInput)
+        {
+            int LuckyNumber = UserInput * 3;
+            return LuckyNumber;
+        }
+
+        /// <summary>
+        /// Receives a user input and subtracts 13 from it to make it unlucky
+        /// </summary>
+        /// <returns>An unlucky version of the input number (-13)</returns>
+        /// <example>
+        /// GET api/Inputs/Unlucky?UserInput=80 -> 67
+        /// GET api/Inputs/Unlucky?UserInput=0 -> -13
+        /// </example>
+        [HttpGet(template:"Unlucky")]
+        public int Unlucky(int UserInput)
+        {
+            int UnluckyNumber = UserInput - 13;
+            return UnluckyNumber;
+        }
+
+        /// <summary>
+        /// Receives two values (base) and (exponent) and returns base^exponent
+        /// </summary>
+        /// <param name="BaseNumber">The base number to raise to an exponent</param>
+        /// <param name="ExponentNumber">The power to raise to</param>
+        /// <returns>
+        /// the result of base to the power of exponent
+        /// </returns>
+        /// <example>
+        /// GET: localhost:xx/api/exponent?BaseNumber=10&ExponentNumber=1 -> 10
+        /// GET: localhost:xx/api/exponent?BaseNumber=10&ExponentNumber=2 -> 100
+        /// 
+        /// GET: localhost:xx/api/exponent?BaseNumber=10&ExponentNumber=1 -> "The base is 10 and the exponent is 1"
+        /// </example>
+        [HttpGet(template:"Exponent")]
+        public double Exponent(int BaseNumber, int ExponentNumber)
+        {
+            double result = Math.Pow(BaseNumber,ExponentNumber);
+            return result;
+        }
+
+
+        /// <summary>
+        /// Receives a POST request with a password
+        /// </summary>
+        /// <param name="Password">The password to identify</param>
+        /// <returns>A sentence acknowledging the password</returns>
+        /// <example>
+        /// POST: api/Inputs/Password
+        //  HEADER: Content-Type: application/x-www-form-urlencoded
+        //  BODY: Password=seasme
+        // -> the password is seasme
+
+        //curl -H "Content-Type: application/x-www-form-urlencoded" -d "Password=test" "https://localhost:7132/api/inputs/password"
+        /// </example>
+        [HttpPost(template:"Password")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public string Password([FromForm]string Password)
+        {
+            return "the password is "+Password;
+        }
+
+
+    }
+}
---
+++ RouteExample/Controllers/ReviewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RouteExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+
+        // GET : localhost:xx/api/Review/Review1 -> "Hello"
+        [HttpGet(template:"Review1")]
+        public string Review1()
+        {
+            return "Hello";
+        }
+
+        // POST : localhost:xx/api/Review/Review2 -> "POST request"
+        [HttpPost(template:"Review2")]
+        public string Review2()
+        {
+            return "POST request";
+        }
+
+        // GET : localhost:xx/api/Review/Review3 -> "Another GET"
+        [HttpGet(template:"Review3")]
+        public string Review3()
+        {
+            return "Another GET";
+        }
+
+    }
+}
